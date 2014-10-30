62f622c64e19e834dd588fe8ac9e87dcc6b8383a
James Topham <apple@Apples-MacBook-Pro.local>
2014-10-30 11:33:44 +0000

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -12,6 +12,8 @@
 -(void)update:(CCTime)delta
 {
     // put update code here
+    character = (Character*)[CCBReader load:character];
+    [physicsNode addChild:character];
 }
 
 // put new methods here
